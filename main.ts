a4f9194e2391bcce8c7fd48ca92f7671d246f307
lorena202326 <139284231+lorena202326@users.noreply.github.com>
2023-07-11 18:32:23 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Butterfly)
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Skull)
+})
